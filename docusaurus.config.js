658f401c018866d21f73c340d7e2860dad2855f7
Lance <lancepage99@gmail.com>
2025-09-29 20:03:59 -0500

    Fix: use CommonJS export for Docusaurus v2 config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,1 +1,35 @@
-export default { title:'NTK Docs', url:'https://6ftdotexe.github.io', baseUrl:'/ntk-docs/', favicon:'img/logo.png' };
+// @ts-check
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'NTK Docs',
+  url: 'https://6ftdotexe.github.io',
+  baseUrl: '/ntk-docs/',
+  favicon: 'img/logo.png',
+
+  themeConfig: {
+    navbar: {
+      title: 'NTK',
+      logo: { alt: 'NTK', src: 'img/logo.png' },
+      items: [
+        { type: 'docSidebar', sidebarId: 'tutorialSidebar', label: 'Docs', position: 'left' },
+        { to: '/docs/CHANGELOG', label: 'Changelog', position: 'left' },
+        {
+          href: 'https://github.com/6ftdotexe/ntk-docs/releases/latest/download/ntk-docs-package.zip',
+          label: 'Download',
+          position: 'right'
+        },
+        { href: 'https://github.com/6ftdotexe/ntk-docs', label: 'GitHub', position: 'right' },
+      ],
+    },
+  },
+
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'),
+      { indexDocs: true, indexPages: true },
+    ],
+  ],
+};
+
+module.exports = config;
